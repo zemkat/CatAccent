6cc0b1ba387eb99b83af85d2a4992cf26793c34d
Kathryn Lybarger <github@zemkat.org>
2015-01-21 16:03:00 -0500

    improve documentation

--- CatAccent.user.js
+++ CatAccent.user.js
@@ -4,11 +4,12 @@
 // @namespace   http://zemkat.org/
 // @include     *
 // @version     1.0
-// @icon		http://zemkat.org/toys/CatAccent/cat32.png
+// @icon	http://zemkat.org/toys/CatAccent/cat32.png
 // @updateURL   http://zemkat.org/toys/CatAccent/CatAccent.meta.js
 // @downloadURL   http://zemkat.org/toys/CatAccent/CatAccent.user.js
 // ==/UserScript==
 
+/* THIS FUNCTION NEEDS MANY IMPROVEMENTS! :) */
 function cat_translate() { /* regexes for cat translations */
     add_to_list(/\bnow\b/gi, "meow", true);
     add_to_list(/\bnew\b/gi, "mew", true);
@@ -16,30 +17,33 @@ function cat_translate() { /* regexes for cat translations */
     add_to_list(/\bhis\b/gi, "hiss", true);
 }
 
-var cat_match = [];
-var cat_replace = [];
-var cat_case = [];
-cat_translate();
+var cat_match = []; /* regexes to match */
+var cat_replace = []; /* what to replace them with */
+var cat_case = []; /* try to match case of source? */
 
+cat_translate(); /* populate arrays */
+
+/* add a rule to the arrays */
 function add_to_list(regex_match, regex_replace, match_case) {
     cat_match.push(regex_match);
     cat_replace.push(regex_replace);
     cat_case.push(match_case);
 }
 
+/* replace string with cat version */
 function cat_string(text) {
     for (j=0;j<=cat_match.length;j++) {
 		if (cat_case[j]) {
-        	text = text.replace(cat_match[j],function(w) { return alter_case(w,cat_replace[j]); });
+        		text = text.replace(cat_match[j],function(w) { return alter_case(w,cat_replace[j]); });
 		} else {
-        	text = text.replace(cat_match[j],cat_replace[j]);
+        		text = text.replace(cat_match[j],cat_replace[j]);
 		}
     }
     return text;
 }
 
-function alter_case(source,target) {
-	// returns target, with its capitalization similar to source
+/* return target, with capitalization similar to source */
+function alter_case(source, target) {
 	if (source[0].toLowerCase() != source[0]) { // first letter is capital
 		if (source.length == 1) return target.toUpperCase();
 		if (source[1].toLowerCase() != source[1]) { // near enough to all caps
@@ -52,19 +56,21 @@ function alter_case(source,target) {
 	}
 }
 
+/* apply cat accent to the tree */
 function cat_page(node) {
     var treeWalker = document.createTreeWalker(
-        node,
-        NodeFilter.SHOW_TEXT,
-        null,
-        false
+        node, /* root */
+        NodeFilter.SHOW_TEXT, /* nodes to show */
+        null, /* custom filter? */
+        false /* expand entity references? */
     );
 
+    /* traverse the tree */
     while(treeWalker.nextNode()) {
         var current = treeWalker.currentNode;
         current.textContent = cat_string(current.textContent);
     }
 }
 
-cat_page(document.body);
+cat_page(document.body); /* do it! */
 
